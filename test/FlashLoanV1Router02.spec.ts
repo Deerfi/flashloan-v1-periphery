682ec267edfd030948b5a73d7f1906a847f43cfc
awakexzxy <awakexzxy@163.com>
2021-03-05 20:40:10 +0800

    Update unit test.

--- test/FlashLoanV1Router02.spec.ts
+++ test/FlashLoanV1Router02.spec.ts
@@ -1,12 +1,12 @@
 import chai, { expect } from 'chai'
 import { Contract } from 'ethers'
 import { AddressZero, Zero, MaxUint256 } from 'ethers/constants'
-import { BigNumber, defaultAbiCoder } from 'ethers/utils'
+import { bigNumberify } from 'ethers/utils'
 import { solidity, MockProvider, createFixtureLoader } from 'ethereum-waffle'
 import { ecsign } from 'ethereumjs-util'
 
 import { expandTo18Decimals, getApprovalDigest, mineBlock, MINIMUM_LIQUIDITY } from './shared/utilities'
-import { router02Fixture } from './shared/fixtures'
+import { V1Fixture } from './shared/fixtures'
 
 chai.use(solidity)
 
@@ -14,7 +14,7 @@ const overrides = {
   gasLimit: 9999999
 }
 
-describe('FlashLoanV1Router01', () => {
+describe('FlashLoanV1Router02', () => {
   const provider = new MockProvider({
     hardfork: 'istanbul',
     mnemonic: 'horn horn horn horn horn horn horn horn horn horn horn horn',
@@ -31,287 +31,122 @@ describe('FlashLoanV1Router01', () => {
   let WETHPool: Contract
   let receiver: Contract
   beforeEach(async function() {
-    const fixture = await loadFixture(router02Fixture)
+    const fixture = await loadFixture(V1Fixture)
     token = fixture.token
     WETH = fixture.WETH
     factory = fixture.factory
-    router = fixture.router
+    router = fixture.router02
     pool = fixture.pool
     WETHPool = fixture.WETHPool
-    receiver = fixture.receiver
+    receiver = fixture.receiver02
   })
 
   afterEach(async function() {
     expect(await provider.getBalance(router.address)).to.eq(Zero)
   })
 
-  describe('Router01', () => {
-    it('factory, WETH', async () => {
-      expect(await router.factory()).to.eq(factory.address)
-      expect(await router.WETH()).to.eq(WETH.address)
-    })
+  describe('Router02', () => {
+    it('flashLoan: token', async () => {
+      const loanAmount = expandTo18Decimals(10000)
+      const premiumAmount = await router.flashFee(token.address, loanAmount)
 
-    it('addLiquidity', async () => {
-      const tokenAmount = expandTo18Decimals(1)
+      await token.transfer(pool.address, loanAmount)
+      await token.transfer(receiver.address, premiumAmount)
 
-      const expectedLiquidity = expandTo18Decimals(1)
-      await token.approve(router.address, MaxUint256)
-      await expect(
-        router.addLiquidity(
-          token.address,
-          tokenAmount,
-          wallet.address,
-          MaxUint256,
-          overrides
-        )
-      )
+      await expect(receiver.flashBorrow(router.address, token.address, loanAmount))
         .to.emit(token, 'Transfer')
-        .withArgs(wallet.address, pool.address, tokenAmount)
-        .to.emit(pool, 'Transfer')
-        .withArgs(AddressZero, AddressZero, MINIMUM_LIQUIDITY)
-        .to.emit(pool, 'Transfer')
-        .withArgs(AddressZero, wallet.address, expectedLiquidity.sub(MINIMUM_LIQUIDITY))
-        .to.emit(pool, 'Sync')
-        .withArgs(tokenAmount)
-        .to.emit(pool, 'Mint')
-        .withArgs(router.address, tokenAmount)
-
-      expect(await pool.balanceOf(wallet.address)).to.eq(expectedLiquidity.sub(MINIMUM_LIQUIDITY))
-    })
-
-    it('addLiquidityETH', async () => {
-      const ETHAmount = expandTo18Decimals(1)
-
-      const expectedLiquidity = expandTo18Decimals(1)
-      await expect(
-        router.addLiquidityETH(
-          wallet.address,
-          MaxUint256,
-          { ...overrides, value: ETHAmount }
-        )
-      )
-        .to.emit(WETHPool, 'Transfer')
-        .withArgs(AddressZero, AddressZero, MINIMUM_LIQUIDITY)
-        .to.emit(WETHPool, 'Transfer')
-        .withArgs(AddressZero, wallet.address, expectedLiquidity.sub(MINIMUM_LIQUIDITY))
-        .to.emit(WETHPool, 'Sync')
-        .withArgs(ETHAmount)
-        .to.emit(WETHPool, 'Mint')
-        .withArgs(
-          router.address,
-          ETHAmount
-        )
-
-      expect(await WETHPool.balanceOf(wallet.address)).to.eq(expectedLiquidity.sub(MINIMUM_LIQUIDITY))
-    })
-
-    async function addLiquidity(tokenAmount: BigNumber) {
-      await token.transfer(pool.address, tokenAmount)
-      await pool.mint(wallet.address, overrides)
-    }
-    it('removeLiquidity', async () => {
-      const tokenAmount = expandTo18Decimals(1)
-      await addLiquidity(tokenAmount)
-
-      const expectedLiquidity = expandTo18Decimals(1)
-      await pool.approve(router.address, MaxUint256)
-      await expect(
-        router.removeLiquidity(
-          token.address,
-          expectedLiquidity.sub(MINIMUM_LIQUIDITY),
-          wallet.address,
-          MaxUint256,
-          overrides
-        )
-      )
-        .to.emit(pool, 'Transfer')
-        .withArgs(wallet.address, pool.address, expectedLiquidity.sub(MINIMUM_LIQUIDITY))
-        .to.emit(pool, 'Transfer')
-        .withArgs(pool.address, AddressZero, expectedLiquidity.sub(MINIMUM_LIQUIDITY))
+        .withArgs(pool.address, router.address, loanAmount)
         .to.emit(token, 'Transfer')
-        .withArgs(pool.address, wallet.address, tokenAmount.sub(1000))
+        .withArgs(router.address, receiver.address, loanAmount)
+        .to.emit(token, 'Transfer')
+        .withArgs(receiver.address, pool.address, loanAmount.add(premiumAmount))
         .to.emit(pool, 'Sync')
-        .withArgs(1000)
-        .to.emit(pool, 'Burn')
-        .withArgs(router.address, tokenAmount.sub(1000), wallet.address)
+        .withArgs(loanAmount.add(premiumAmount))
+        .to.emit(pool, 'FlashLoan')
+        .withArgs(router.address, router.address, token.address, loanAmount, premiumAmount)
 
-      expect(await pool.balanceOf(wallet.address)).to.eq(0)
-      const totalSupplyToken = await token.totalSupply()
-      expect(await token.balanceOf(wallet.address)).to.eq(totalSupplyToken.sub(1000))
+        const reserve = await pool.reserve()
+        expect(reserve).to.eq(loanAmount.add(premiumAmount))
+        expect(await token.balanceOf(pool.address)).to.eq(loanAmount.add(premiumAmount))
+        expect(await token.balanceOf(receiver.address)).to.eq(0)
+        const totalSupplyToken = await token.totalSupply()
+        expect(await token.balanceOf(wallet.address)).to.eq(totalSupplyToken.sub(loanAmount).sub(premiumAmount))
     })
 
-    it('removeLiquidityETH', async () => {
-      const ETHAmount = expandTo18Decimals(1)
+    it('flashLoan: eth', async () => {
+      const loanAmount = expandTo18Decimals(10000)
+      const premiumAmount = await router.flashFee(token.address, loanAmount)
+      const ETHAmount = expandTo18Decimals(10005)
+
       await WETH.deposit({ value: ETHAmount })
-      await WETH.transfer(WETHPool.address, ETHAmount)
-      await WETHPool.mint(wallet.address, overrides)
+      await WETH.transfer(WETHPool.address, loanAmount)
+      await WETH.transfer(receiver.address, premiumAmount)
 
-      const expectedLiquidity = expandTo18Decimals(1)
-      await WETHPool.approve(router.address, MaxUint256)
-      await expect(
-        router.removeLiquidityETH(
-          expectedLiquidity.sub(MINIMUM_LIQUIDITY),
-          wallet.address,
-          MaxUint256,
-          overrides
-        )
-      )
-        .to.emit(WETHPool, 'Transfer')
-        .withArgs(wallet.address, WETHPool.address, expectedLiquidity.sub(MINIMUM_LIQUIDITY))
-        .to.emit(WETHPool, 'Transfer')
-        .withArgs(WETHPool.address, AddressZero, expectedLiquidity.sub(MINIMUM_LIQUIDITY))
+      await expect(receiver.flashBorrow(router.address, WETH.address, loanAmount))
         .to.emit(WETH, 'Transfer')
-        .withArgs(WETHPool.address, router.address, ETHAmount.sub(1000))
+        .withArgs(WETHPool.address, router.address, loanAmount)
+        .to.emit(WETH, 'Transfer')
+        .withArgs(router.address, receiver.address, loanAmount)
+        .to.emit(WETH, 'Transfer')
+        .withArgs(receiver.address, WETHPool.address, loanAmount.add(premiumAmount))
         .to.emit(WETHPool, 'Sync')
-        .withArgs(1000)
-        .to.emit(WETHPool, 'Burn')
-        .withArgs(
-          router.address,
-          ETHAmount.sub(1000),
-          router.address
-        )
-
-      expect(await WETHPool.balanceOf(wallet.address)).to.eq(0)
-      const totalSupplyWETH = await WETH.totalSupply()
-      expect(await WETH.balanceOf(wallet.address)).to.eq(totalSupplyWETH.sub(1000))
+        .withArgs(loanAmount.add(premiumAmount))
+        .to.emit(WETHPool, 'FlashLoan')
+        .withArgs(router.address, router.address, WETH.address, loanAmount, premiumAmount)
+  
+        const reserve = await WETHPool.reserve()
+        expect(reserve).to.eq(loanAmount.add(premiumAmount))
+        expect(await WETH.balanceOf(WETHPool.address)).to.eq(loanAmount.add(premiumAmount))
+        expect(await WETH.balanceOf(receiver.address)).to.eq(0)
+        const totalSupplyToken = await WETH.totalSupply()
+        expect(await WETH.balanceOf(wallet.address)).to.eq(totalSupplyToken.sub(loanAmount).sub(premiumAmount))
     })
 
-    it('removeLiquidityWithPermit', async () => {
-      const tokenAmount = expandTo18Decimals(1)
-      await addLiquidity(tokenAmount)
+    it('flashloan: maxFlashLoan', async () => {
+      const loanAmount = expandTo18Decimals(10000)
+      const premiumAmount = await router.flashFee(token.address, loanAmount)
+      const one = bigNumberify(1)
+      const maxFlashLoanAmountAfter = expandTo18Decimals(10005)
 
-      const expectedLiquidity = expandTo18Decimals(1)
+      await mineBlock(provider, (await provider.getBlock('latest')).timestamp + 1)
+      await pool.sync(overrides)
 
-      const nonce = await pool.nonces(wallet.address)
-      const digest = await getApprovalDigest(
-        pool,
-        { owner: wallet.address, spender: router.address, value: expectedLiquidity.sub(MINIMUM_LIQUIDITY) },
-        nonce,
-        MaxUint256
-      )
+      await token.transfer(pool.address, loanAmount)
+      await token.transfer(receiver.address, premiumAmount)
 
-      const { v, r, s } = ecsign(Buffer.from(digest.slice(2), 'hex'), Buffer.from(wallet.privateKey.slice(2), 'hex'))
+      const maxFlashLoanAmount = await router.maxFlashLoan(token.address)
 
-      await router.removeLiquidityWithPermit(
-        token.address,
-        expectedLiquidity.sub(MINIMUM_LIQUIDITY),
-        wallet.address,
-        MaxUint256,
-        false,
-        v,
-        r,
-        s,
-        overrides
-      )
+      await mineBlock(provider, (await provider.getBlock('latest')).timestamp + 1)
+      expect(await token.balanceOf(pool.address)).to.eq(loanAmount)
+      expect(await token.balanceOf(pool.address)).to.eq(maxFlashLoanAmount.add(one))
+
+      await receiver.flashBorrow(router.address, token.address, loanAmount)
+      expect(await token.balanceOf(pool.address)).to.eq(maxFlashLoanAmountAfter)
     })
 
-    it('removeLiquidityETHWithPermit', async () => {
-      const ETHAmount = expandTo18Decimals(1)
-      await WETH.deposit({ value: ETHAmount })
-      await WETH.transfer(WETHPool.address, ETHAmount)
-      await WETHPool.mint(wallet.address, overrides)
+    it('flashloan: flashFee', async () => {
+      const loanAmount = expandTo18Decimals(10000)
+      const premiumAmount = router.flashFee(token.address, loanAmount)
+  
+      await mineBlock(provider, (await provider.getBlock('latest')).timestamp + 1)
+      await pool.sync(overrides)
 
-      const expectedLiquidity = expandTo18Decimals(1)
-
-      const nonce = await WETHPool.nonces(wallet.address)
-      const digest = await getApprovalDigest(
-        WETHPool,
-        { owner: wallet.address, spender: router.address, value: expectedLiquidity.sub(MINIMUM_LIQUIDITY) },
-        nonce,
-        MaxUint256
-      )
-
-      const { v, r, s } = ecsign(Buffer.from(digest.slice(2), 'hex'), Buffer.from(wallet.privateKey.slice(2), 'hex'))
-
-      await router.removeLiquidityETHWithPermit(
-        expectedLiquidity.sub(MINIMUM_LIQUIDITY),
-        wallet.address,
-        MaxUint256,
-        false,
-        v,
-        r,
-        s,
-        overrides
-      )
+      expect(await premiumAmount).to.eq(expandTo18Decimals(5))
     })
 
-    describe('flashLoan', () => {
-      it('flashLoan: token', async () => {
-        const loanAmount = expandTo18Decimals(10000)
-        const premiumAmount = expandTo18Decimals(5)
+    it('flashloan: gas', async () => {
+      const loanAmount = expandTo18Decimals(10000)
+      const premiumAmount = router.flashFee(token.address, loanAmount)
 
-        await token.transfer(pool.address, loanAmount)
-        await token.transfer(receiver.address, premiumAmount)
+      await mineBlock(provider, (await provider.getBlock('latest')).timestamp + 1)
+      await pool.sync(overrides)
 
-        await expect(receiver.flashBorrow(router.address, token.address, loanAmount))
-          .to.emit(token, 'Transfer')
-          .withArgs(pool.address, router.address, loanAmount)
-          .to.emit(token, 'Transfer')
-          .withArgs(router.address, receiver.address, loanAmount)
-          .to.emit(token, 'Transfer')
-          .withArgs(receiver.address, pool.address, loanAmount.add(premiumAmount))
-          .to.emit(pool, 'Sync')
-          .withArgs(loanAmount.add(premiumAmount))
-          .to.emit(pool, 'FlashLoan')
-          .withArgs(router.address, router.address, token.address, loanAmount, premiumAmount)
-
-          const reserve = await pool.reserve()
-          expect(reserve).to.eq(loanAmount.add(premiumAmount))
-          expect(await token.balanceOf(pool.address)).to.eq(loanAmount.add(premiumAmount))
-          expect(await token.balanceOf(receiver.address)).to.eq(0)
-          const totalSupplyToken = await token.totalSupply()
-          expect(await token.balanceOf(wallet.address)).to.eq(totalSupplyToken.sub(loanAmount).sub(premiumAmount))
-      })
-
-      it('flashLoan: eth', async () => {
-        const loanAmount = expandTo18Decimals(10000)
-        const premiumAmount = expandTo18Decimals(5)
-        const ETHAmount = expandTo18Decimals(10005)
-
-        const data = defaultAbiCoder.encode(
-          ['address'],
-          [WETHPool.address]
-        )
-
-        await WETH.deposit({ value: ETHAmount })
-        await WETH.transfer(WETHPool.address, loanAmount)
-        await WETH.transfer(receiver.address, premiumAmount)
-        
-        await expect(receiver.flashBorrow(router.address, WETH.address, loanAmount))
-          .to.emit(WETH, 'Transfer')
-          .withArgs(WETHPool.address, router.address, loanAmount)
-          .to.emit(WETH, 'Transfer')
-          .withArgs(router.address, receiver.address, loanAmount)
-          .to.emit(WETH, 'Transfer')
-          .withArgs(receiver.address, WETHPool.address, loanAmount.add(premiumAmount))
-          .to.emit(WETHPool, 'Sync')
-          .withArgs(loanAmount.add(premiumAmount))
-          .to.emit(WETHPool, 'FlashLoan')
-          .withArgs(router.address, router.address, WETH.address, loanAmount, premiumAmount)
-    
-          const reserve = await WETHPool.reserve()
-          expect(reserve).to.eq(loanAmount.add(premiumAmount))
-          expect(await WETH.balanceOf(WETHPool.address)).to.eq(loanAmount.add(premiumAmount))
-          expect(await WETH.balanceOf(receiver.address)).to.eq(0)
-          const totalSupplyToken = await WETH.totalSupply()
-          expect(await WETH.balanceOf(wallet.address)).to.eq(totalSupplyToken.sub(loanAmount).sub(premiumAmount))
-      })
-    
-      it('flashloan:gas', async () => {
-        const loanAmount = expandTo18Decimals(10000)
-        const premiumAmount = expandTo18Decimals(5)
-    
-        // ensure that setting price{0,1}CumulativeLast for the first time doesn't affect our gas math
-        await mineBlock(provider, (await provider.getBlock('latest')).timestamp + 1)
-        await pool.sync(overrides)
-
-        await token.transfer(pool.address, loanAmount)
-        await token.transfer(receiver.address, premiumAmount)
-        await mineBlock(provider, (await provider.getBlock('latest')).timestamp + 1)
-        const tx = await receiver.flashBorrow(router.address, token.address, loanAmount)
-        const receipt = await tx.wait()
-        expect(receipt.gasUsed).to.eq(228728)
-      })
+      await token.transfer(pool.address, loanAmount)
+      await token.transfer(receiver.address, premiumAmount)
+      await mineBlock(provider, (await provider.getBlock('latest')).timestamp + 1)
+      const tx = await receiver.flashBorrow(router.address, token.address, loanAmount)
+      const receipt = await tx.wait()
+      expect(receipt.gasUsed).to.eq(228552)
     })
   })
 })
--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -12,7 +12,7 @@ import WETH9 from '../../build/WETH9.json'
 import FlashLoanV1Router01 from '../../build/FlashLoanV1Router01.json'
 import FlashLoanV1Router02 from '../../build/FlashLoanV1Router02.json'
 import FlashLoanReceiver from '../../build/FlashLoanReceiver.json'
-import FlashLoanReceiver01 from '../../build/FlashLoanReceiver01.json'
+import FlashLoanReceiver02 from '../../build/FlashLoanReceiver02.json'
 
 const overrides = {
   gasLimit: 9999999
@@ -22,58 +22,29 @@ interface Router01Fixture {
   token: Contract
   WETH: Contract
   factory: Contract
+  router01: Contract
+  router02: Contract
   router: Contract
   pool: Contract
   WETHPool: Contract
-  receiver: Contract
+  receiver01: Contract
+  receiver02: Contract
 }
 
-export async function router01Fixture(provider: Web3Provider, [wallet]: Wallet[]): Promise<Router01Fixture> {
+export async function V1Fixture(provider: Web3Provider, [wallet]: Wallet[]): Promise<Router01Fixture> {
   // deploy tokens
   const token = await deployContract(wallet, ERC20, [expandTo18Decimals(10005)])
   const WETH = await deployContract(wallet, WETH9)
 
   // deploy receiver
-  const receiver = await deployContract(wallet, FlashLoanReceiver01)
+  const receiver01 = await deployContract(wallet, FlashLoanReceiver)
+  const receiver02 = await deployContract(wallet, FlashLoanReceiver02)
 
   // deploy factory
   const factory = await deployContract(wallet, FlashLoanV1Factory, [wallet.address])
 
   // deploy router
   const router01 = await deployContract(wallet, FlashLoanV1Router01, [factory.address, WETH.address], overrides)
-
-  // initialize V1
-  await factory.createPool(token.address)
-  const tokenAddress = await factory.getPool(token.address)
-  const pool = new Contract(tokenAddress, JSON.stringify(IFlashLoanV1Pool.abi), provider).connect(wallet)
-
-  await factory.createPool(WETH.address)
-  const WETHAddress = await factory.getPool(WETH.address)
-  const WETHPool = new Contract(WETHAddress, JSON.stringify(IFlashLoanV1Pool.abi), provider).connect(wallet)
-
-  return {
-    token,
-    WETH,
-    factory,
-    router: router01, // the default router, 01 had a minor bug
-    pool,
-    WETHPool,
-    receiver
-  }
-}
-
-export async function router02Fixture(provider: Web3Provider, [wallet]: Wallet[]): Promise<Router01Fixture> {
-  // deploy tokens
-  const token = await deployContract(wallet, ERC20, [expandTo18Decimals(10005)])
-  const WETH = await deployContract(wallet, WETH9)
-
-  // deploy receiver
-  const receiver = await deployContract(wallet, FlashLoanReceiver)
-
-  // deploy factory
-  const factory = await deployContract(wallet, FlashLoanV1Factory, [wallet.address])
-
-  // deploy router
   const router02 = await deployContract(wallet, FlashLoanV1Router02, [factory.address, WETH.address], overrides)
 
   // initialize V1
@@ -89,9 +60,12 @@ export async function router02Fixture(provider: Web3Provider, [wallet]: Wallet[]
     token,
     WETH,
     factory,
-    router: router02, // the default router, 01 had a minor bug
+    router01,
+    router02,
+    router: router01, // the default router
     pool,
     WETHPool,
-    receiver
+    receiver01,
+    receiver02
   }
 }
